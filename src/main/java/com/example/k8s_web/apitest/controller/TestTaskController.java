1dd3f0ac209d78e291ceb7535c3f57f5f2ef4d27
属语 <1440837150@qq.com>
2023-06-06 16:48:47 +0800

    测试任务接口

--- src/main/java/com/example/k8s_web/apitest/controller/TestTaskController.java
+++ src/main/java/com/example/k8s_web/apitest/controller/TestTaskController.java
@@ -1,10 +1,61 @@
 package com.example.k8s_web.apitest.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.k8s_web.apitest.dto.TestTaskDto;
+import com.example.k8s_web.vo.ApiResult;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/testTask")
 public class TestTaskController {
+    /**
+     * 添加测试任务
+     * @param testTaskDto
+     * @return
+     */
+    @PostMapping("/addTestTask")
+    public ApiResult addTestTask(@RequestBody TestTaskDto testTaskDto) {
+        return null;
+    }
 
+    /**
+     * 获取测试任务列表
+     * @return
+     */
+    @GetMapping("/list")
+    public ApiResult listTask(PageRequest pageRequest){
+        return null;
+    }
+
+    /**
+     * 获取一个测试任务
+     * @param id
+     * @return
+     */
+    @GetMapping("/id")
+    public ApiResult oneTask(Long id){
+        return null;
+    }
+
+    /**
+     * 启动一个测试任务
+     * @return
+     */
+    @GetMapping("/start")
+    public ApiResult runTask(Long id){
+        return null;
+    }
+
+    /**
+     * 停止一个测试任务
+     * @return
+     */
+    @GetMapping("/stop")
+    public ApiResult stopTask(Long id){
+        return null;
+    }
+    @GetMapping("/result")
+    public ApiResult taskResult(Long id){
+        return null;
+    }
 }
---
+++ src/main/java/com/example/k8s_web/apitest/dto/TestTaskDto.java
@@ -0,0 +1,8 @@
+package com.example.k8s_web.apitest.dto;
+
+import lombok.Data;
+
+@Data
+public class TestTaskDto {
+
+}
